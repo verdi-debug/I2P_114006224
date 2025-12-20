ac1aff808267b871095d522ab4a8f932cd6a1ca8
Verdihan <verdihanx@gmail.com>
2025-12-20 22:53:52 +0800

    fifth commit

--- assets/maps/Chicken.tsx
+++ assets/maps/Chicken.tsx
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Chicken" tilewidth="16" tileheight="16" tilecount="16" columns="4">
- <image source="../../../../../Downloads/Cute_Fantasy_Free/Cute_Fantasy_Free/Animals/Chicken/Chicken.png" width="64" height="64"/>
-</tileset>
+<?xml version = "1.0" encoding = "UTF-8" ?>
+    <tileset version="1.10" tiledversion="1.11.2" name="Chicken" tilewidth="16" tileheight="16" tilecount="16" columns="4">
+        <image source="Chicken.png" width="64" height="64" />
+    </tileset>
